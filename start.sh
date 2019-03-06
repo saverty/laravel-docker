f4870d6711b2b9bd50b94cc125339a52873ad12d
Steve Averty <steve.averty1992@gmail.com>
2019-03-06 09:08:06 +0100

    script

--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 echo "Laravel start"
+
+docker run docker/compose:1.13.0 version
+docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "$PWD:/rootfs/$PWD" \
+    -w="/rootfs/$PWD" \
+    docker/compose:1.13.0 up
+
+source ~/.bashrc
+docker-compose build
+docker-compose up
 #php /usr/local/bin/composer.phar install
 cp /var/www/.env.example /var/www/.env
 rm /var/www/.env.example
