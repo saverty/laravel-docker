a57f392f73c661386933b1eb3ecc19417848ccf6
Steve Averty <steve.averty1992@gmail.com>
2019-03-06 09:10:13 +0100

    script

--- start.sh
+++ start.sh
@@ -10,7 +10,13 @@ docker run --rm \
 
 source ~/.bashrc
 docker-compose build
+
+git clone https://github.com/saverty/laravel-docker.git
+
 docker-compose up
+
+
+
 #php /usr/local/bin/composer.phar install
 cp /var/www/.env.example /var/www/.env
 rm /var/www/.env.example
